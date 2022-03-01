34416927ab53be6232f457b1734147b95050c953
Max Morrison <maxrmorrison@gmail.com>
2022-03-01 17:47:33 -0600

    TODO

--- reseval/assets/server/routes/api/evaluators.ts
+++ reseval/assets/server/routes/api/evaluators.ts
@@ -1,8 +1,9 @@
 import * as express from 'express';
-import db from '../../db';
 
 const router = express.Router();
 
+// TODO - If we resume or extend training, can we have this start at the correct ID?
+//        Would need to write current index to database and retrieve it on startup.
 let ID = 0;
 
 router.get('/', async (request, response) => {
