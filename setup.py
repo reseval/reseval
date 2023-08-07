0afed36696b130eb3a65d022b707743c952ae0bc
Max Morrison <maxrmorrison@gmail.com>
2023-08-07 19:17:55 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data():
 setup(
     name='reseval',
     description='Reproducible Subjective Evaluation',
-    version='0.1.0',
+    version='0.1.1',
     author='Max Morrison, Brian Tang, Gefei Tan, Bryan Pardo',
     author_email='maxrmorrison@gmail.com',
     url='https://github.com/reseval/reseval',
